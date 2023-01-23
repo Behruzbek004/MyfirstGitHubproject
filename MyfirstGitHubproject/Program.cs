882e5fba04ba4b75c6bd6dace046e2143c84b448
Behruzbek Jamilov <behruzbekjamilov5@gmail.com>
2023-01-23 18:26:06 +0500

    if Question

--- MyfirstGitHubproject/Program.cs
+++ MyfirstGitHubproject/Program.cs
@@ -1,1 +1,202 @@
-﻿
+﻿Behruz:
+//1-misol
+//int number = int.Parse(Console.ReadLine());
+//if(number>0)
+//{
+//Console.WriteLine(++number);
+//}
+//else
+//{
+//Console.WriteLine(number);
+//}
+//2-misol
+//int number = int.Parse(Console.ReadLine());
+//if (number > 0)
+//{
+//Console.WriteLine(++number);  
+//}
+//else
+//{
+//Console.WriteLine(number - 2);
+//}
+//3-misol
+//int number = int.Parse(Console.ReadLine());
+//if (number > 0)
+//{
+//Console.WriteLine(++number);  
+//}
+//else if (number < 0)
+//{
+//Console.WriteLine(number - 2);
+//}
+//else
+//{
+//number = 10;
+//Console.WriteLine(number);
+//}
+//4-misol
+//int number = int.Parse(Console.ReadLine());
+//int number1 = int.Parse(Console.ReadLine());
+//int number2 = int.Parse(Console.ReadLine());
+//if ((number > 0 && number1 < 0 && number2 < 0) || (number < 0 && number1 > 0 && number2 < 0) || (number < 0 && number1 < 0 && number2 > 0))
+//{
+//Console.WriteLine(" Musbat sonlar soni " + 1 + " ta");
+//}
+//else if ((number > 0 && number1 > 0 && number2 < 0) || (number > 0 && number1 < 0 && number2 > 0) || (number < 0 && number1 > 0 && number2 > 0))
+//{
+//Console.WriteLine(" Musbat sonlar soni " + 2 + " ta");
+//}
+//lse if (number > 0 && number1 > 0 && number2 > 0)
+//{
+//Console.WriteLine(" Musbat sonlar soni " + 3 + " ta ");
+//}
+//5-misol
+//int number = int.Parse(Console.ReadLine());
+//int number1 = int.Parse(Console.ReadLine());
+//int number2 = int.Parse(Console.ReadLine());
+//if ((number > 0 && number1 < 0 && number2 < 0)||(number < 0 && number1 > 0 && number2 < 0)||(number < 0 && number1 < 0 && number2 > 0))
+//{
+//Console.WriteLine(" Musbat sonla soni " + 1 + " ta");
+//Console.WriteLine(" Manfiy sonlar soni " + 2 + " ta");
+//}
+//else if ((number > 0 && number1 > 0 && number2 < 0)||(number < 0 && number1 > 0 && number2 > 0)||(number > 0 && number1 < 0 && number2 > 0))
+//{
+//Console.WriteLine(" Musbat sonlar soni " + 2 + " ta");
+//Console.WriteLine(" Manfiy sonlar soni " + 1 + " ta");
+//}
+//else if ((number > 0 && number1 > 0 && number2 > 0))
+//{
+//Console.WriteLine(" Musbat sonlar soni " + 3 + " ta");
+//Console.WriteLine(" Manfiy sonlar soni " + 0 + " ta");
+//}
+//else
+//{
+//Console.WriteLine(" Manfiy sonlar soni " + 3 + " ta");
+//}
+//6-misol
+//int A = int.Parse(Console.ReadLine());
+//int B = int.Parse(Console.ReadLine());
+//if (A > B)
+//{
+//Console.WriteLine( A );
+//}
+//else
+//{
+//Console.WriteLine( B );
+//}
+//7-misol
+//int A = int.Parse(Console.ReadLine());
+//int B = int.Parse(Console.ReadLine());
+//if (A < B)
+//{
+//Console.WriteLine( A );
+//}
+//else
+//{
+//Console.WriteLine( B );
+//}
+//8-misol
+//int A = int.Parse(Console.ReadLine());
+//int B = int.Parse(Console.ReadLine());
+//if (A > B)
+//{
+//    Console.WriteLine("Max " + A);
+//    Console.WriteLine("Min " + B);
+//}
+//if (A < B)
+//{
+//    Console.WriteLine("Max " + B);
+//    Console.WriteLine("Min " + A);
+//}
+//9-misol
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//if (a > b)
+//{
+//    int c = b;
+//    b = a;
+//    a = c;
+//    Console.WriteLine(" b = " + b + " \n a = " + a);
+//}
+//else if (a < b )
+//{
+//    Console.WriteLine(" b = " + b + " \n a = " + a);
+//}
+//10-misol
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//if (a != b)
+//{
+//    Console.WriteLine("Yig'indi " + (a + b));
+//}
+//else if (a == b)
+//{
+//    Console.WriteLine(0);
+//}
+//11-misol
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//if ((a != b) && (a > b))
+//{
+//    Console.WriteLine("Natija = " + a);
+//}
+//else if ((a != b) && (a < b))
+//{
+//    Console.WriteLine("Natija = " + b);
+//}
+//else
+//{
+//    Console.WriteLine(0);
+//}
+//12-misol
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+goto Behruz;
